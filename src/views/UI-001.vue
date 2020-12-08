6491a4ba015b5de673e4369cc088632a43302ed3
karljame <karl321jame@gmail.com>
2020-12-08 14:08:34 +0200

    karl jäme

--- src/views/UI-001.vue
+++ src/views/UI-001.vue
@@ -1,8 +1,38 @@
 <template>
   <div class="flex-center">
     <div class="home-content">
-      <h1>Siia tuleb esimene ülesanne (sign-up form)</h1>
-      <p>Challenge: Design Hint... Create a sign up page, modal, form, or app screen related to signing up for something. It could be for a volunteer event, contest registration, a giveaway, or anything you can image.</p>
+      <h2>create a new account </h2>
+      <div class="input">
+        <h1>Email</h1>
+      <input type="email">
+      <h1>Full name</h1>
+      <input type="full name">
+      <h1>Username</h1>
+      <input type="username">
+      <h1>Password</h1>
+      <input type="password"> 
+      </div>
+      <p></p>
     </div>
   </div>
 </template>
+
+
+<style lang="scss">
+.input {
+  display:flex;
+  flex-direction: column;
+  
+}
+ input {
+  margin: 5px;
+  padding: 4px;
+ }
+h1 {
+font-size: 15px;
+display: flex;
+}
+h2 {
+  font-size:25px;
+}
+</style>
