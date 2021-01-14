0ecdef8202839697e70ba4822dc9816a33401137
karljame <karl321jame@gmail.com>
2021-01-14 22:34:10 +0200

    done

--- src/views/UI-001.vue
+++ src/views/UI-001.vue
@@ -11,17 +11,27 @@
       <input type="username" placeholder="Username">
 
       <input type="password" placeholder="Password"> 
-
+      
       <button class="button ">Next</button>
+
+      <div class="app">Get this app </div>
+      <div class="rick-roll"></div>
+      <p>
+             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">  
+            <img border="0" alt="W3Schools" class="apple" src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png" width="134" height="40">
+        </a>
+             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">  
+            <img border="0" alt="W3Schools" class="google" src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png" width="134" height="40">
+        </a>
+      </p>
       </div>
-      <p></p>
     </div>
   </div>
 </template>
 
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Alatsi&display=swap');
 .input {
   display:flex;
   flex-direction: column;
@@ -36,14 +46,16 @@ font-size: 15px;
 display: flex;
 }
 h2 {
-  font-size:25px;
+  font-size:29px;
   color: rgba(36, 94, 253, 0.88);
 }
 .home-content {
   border: gray 2px solid;
-  font-family: 'Open Sans', sans-serif;
+  font-family: 'Alatsi', sans-serif;
   border-radius: 3px;
+  background: white;
   box-shadow: 0 0 30px rgba(0, 0, 0, 0.50);
+  
 }
 .button {
   color: white;
@@ -52,7 +64,27 @@ h2 {
   margin-top: 15px;
   margin-left:225px ;
   margin-right: 15px;
-  font-family: 'Open Sans', sans-serif;
+  font-family: 'Alatsi', sans-serif;
   border-radius: 7px;
+  
 }
+.app  {
+  margin-top: 2rem;
+}
+.rick-roll {
+ margin-top:5px ;
+}
+p {
+  display: flex;
+  justify-content: center;
+
+}
+body {
+  background-color: rgb(123, 76, 224);
+}
+.left-container {
+  left: 50px;
+}
+
+
 </style>
--- src/views/UI-002.vue
+++ src/views/UI-002.vue
@@ -1,16 +1,29 @@
 <template>
+<div>
   <div class="coming-soon ">
-      <h1>Comeing soon</h1>
+      <h1>Coming soon</h1>
   </div>
+  <p>Never</p>
+</div>
 </template>
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap');
 .coming-soon {
     display: flex;
     justify-content: center;
-    color: rgba(0, 0, 255, 0.637);
+    color: rgb(123, 76, 224);
     align-items: center;
      height: 100vh;
      width: 100vw;
-}
+     font-size: 100px;
 
+}
+p {
+  font-size: 100px;
+  color: rgb(4, 178, 209);
+ font-family: 'Alfa Slab One', cursive;
+}
+body {
+  background: black;
+}
 </style>
